8fe4736d4eb382e0b12fcac4df0eac9bed20664e
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-01-26 16:20:31 +0330

    extend tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,29 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#F17228",
+        secondary: "#FFB30E",
+        gray: {
+          100: "#9A9898",
+          200: "#434343",
+        },
+        white: "#ffffff",
       },
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
   },
   plugins: [],
