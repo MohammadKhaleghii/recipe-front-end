193e63282c6d4a1b51b3b755b994f441f5e790d1
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-01-29 09:28:54 +0330

    add badge styled component

---
+++ src/styles/components/badge/index.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const RecipeBadge = styled.div`
+  padding: 5px;
+  border: 1px solid #e2e2e2;
+  border-radius: 5px;
+  width: fit-content;
+  background-color: #d5d4d4;
+  color: #292929;
+`;
