806eff7d66467c504d9c2b427de9702371913f4e
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-01-29 09:26:08 +0330

    add recipe card component

---
+++ src/components/recipe-card/index.tsx
@@ -0,0 +1,8 @@
+import {FC} from "react";
+import {RecipeCardProps} from "./recipe-card.interface";
+
+const RecipeCard: FC<RecipeCardProps> = () => {
+  return <></>;
+};
+
+export default RecipeCard;
