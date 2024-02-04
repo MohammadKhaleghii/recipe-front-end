29647a7dcfad88ddab56d19acfbca9674c0671db
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-02-04 14:29:26 +0330

    update recipe item skeleton height

--- src/components/recipe-item/recipe-item-skeleton.tsx
+++ src/components/recipe-item/recipe-item-skeleton.tsx
@@ -5,7 +5,7 @@ import Skeleton from "react-loading-skeleton";
 
 const RecipeItemSkeleton = () => {
   return (
-    <Skeleton className="flex items-center justify-center flex-col w-auto h-80 "></Skeleton>
+    <Skeleton className="flex items-center justify-center flex-col w-auto lg:h-80 h-40 "></Skeleton>
   );
 };
 
