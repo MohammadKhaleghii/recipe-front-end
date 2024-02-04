835f1f1e3951f1aa821713d816b68e0ecfa2ac2e
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-02-04 14:32:26 +0330

    update recipe item styles

--- src/components/recipe-item/index.tsx
+++ src/components/recipe-item/index.tsx
@@ -10,12 +10,12 @@ const RecipeItem: FC<RecipeHit> = ({_links, recipe}) => {
   return (
     <Link
       href={`/details/${recipeID[1]}`}
-      className="flex relative items-center justify-center flex-col h-auto w-auto group transition delay-150 lg:min-h-80 "
+      className="flex relative items-center justify-center flex-col h-auto w-auto group transition delay-150 lg:min-h-80 min-h-32 "
     >
       <img
         src={recipe.image}
         alt={recipe.label}
-        className="rounded-md w-full hover:blur-sm duration-300 transition-all"
+        className="rounded-md w-full hover:blur-sm duration-300 transition-all relative"
       />
 
       <div className="absolute w-full h-full bg-black opacity-20 hover:bg-secondary rounded-lg"></div>
