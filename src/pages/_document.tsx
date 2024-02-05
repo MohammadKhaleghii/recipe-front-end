fc77979a39d0f575353f905d7871c48b6d84b508
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-02-05 15:47:32 +0330

    update manifest file path

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="application-name" content="Recipe" />
         <meta name="apple-mobile-web-app-title" content="Recipe" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="manifest.json" />
       </Head>
       <body>
         <Main />
