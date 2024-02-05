e4eb27446da5b67a0382d02226ad1dc853762941
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-02-05 11:33:27 +0330

    add filter functionality for search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -11,7 +11,17 @@ import {ReactElement, useEffect, useState} from "react";
 
 const RecipeSearchPage = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(true);
+  let [selectedDietFilter, setSelectedDietFilter] = useState<string[]>([]);
+  let [selectedHealthFilter, setSelectedHealthFilter] = useState<string[]>([]);
+  let [selectedCuisineTypeFilter, setSelectedCuisineTypeFilter] = useState<
+    string[]
+  >([]);
+
   const [recipeSearchItems, setRecipeSearchItems] = useState<RecipeSearch>();
+  const [searchedQuery, setSearchedQuery] = useState(
+    router.query.q?.toString() ?? ""
+  );
 
   const muckArrayForSkeleton = new Array(9).fill(0);
   useEffect(() => {
@@ -19,8 +29,15 @@ const RecipeSearchPage = () => {
       beta: false,
       imageSize: "LARGE",
       type: "any",
+      diet: "balanced",
       q: router.query.q?.toString() ?? "",
     };
+    // if (true) {
+    //   const diet = router.query.diet?.toString().split(",");
+    //   console.log(diet);
+    //   searchParams.diet = diet;
+    // }
+    setLoading(true);
     getRecipeSearch(searchParams)
       .then(({data}) => {
         const recipeItemsTenFirstItems = data.hits.slice(0, 41);
@@ -29,11 +46,13 @@ const RecipeSearchPage = () => {
           hits: recipeItemsTenFirstItems,
         };
         setRecipeSearchItems(data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error(error);
+        setLoading(false);
       });
-  }, []);
+  }, [router.query]);
   const sidebarFilters = [
     {
       title: "Diet",
@@ -110,6 +129,83 @@ const RecipeSearchPage = () => {
       ],
     },
   ];
+  const handleSearchFilters = (parentKey: number, key: string) => {
+    switch (parentKey) {
+      case 1:
+        const findDietEl = selectedDietFilter.find((item) => item === key);
+        if (findDietEl) {
+          const newDietArray = selectedDietFilter.filter(
+            (item) => item !== key
+          );
+          setSelectedDietFilter(newDietArray);
+          break;
+        }
+        setSelectedDietFilter([...selectedDietFilter, key]);
+        break;
+      case 2:
+        const findHealthEl = selectedHealthFilter.find((item) => item === key);
+        if (findHealthEl) {
+          const newHealthArray = selectedHealthFilter.filter(
+            (item) => item !== key
+          );
+          setSelectedHealthFilter(newHealthArray);
+          break;
+        }
+        setSelectedHealthFilter([...selectedHealthFilter, key]);
+        break;
+      case 3:
+        const findCuisineType = selectedCuisineTypeFilter.find(
+          (item) => item === key
+        );
+        if (findCuisineType) {
+          const newCuisineTypeArray = selectedCuisineTypeFilter.filter(
+            (item) => item !== key
+          );
+          setSelectedCuisineTypeFilter(newCuisineTypeArray);
+          break;
+        }
+
+        setSelectedCuisineTypeFilter([...selectedCuisineTypeFilter, key]);
+        break;
+    }
+  };
+  const handleUpdateChecklistIcon = (parentKey: number, key: string) => {
+    switch (parentKey) {
+      case 1:
+        const findDietEl = selectedDietFilter.find((item) => item === key);
+        if (findDietEl) {
+          return false;
+        }
+        return true;
+      case 2:
+        const findHealthEl = selectedHealthFilter.find((item) => item === key);
+        if (findHealthEl) {
+          return false;
+        }
+        return true;
+
+      case 3:
+        const findCuisineType = selectedCuisineTypeFilter.find(
+          (item) => item === key
+        );
+        if (findCuisineType) {
+          return false;
+        }
+
+        return true;
+    }
+  };
+
+  const handleApplyFilter = () => {
+    router.push({
+      query: {
+        diet: selectedDietFilter.toString(),
+        health: selectedHealthFilter.toString(),
+        cuisineType: selectedCuisineTypeFilter.toString(),
+      },
+    });
+  };
+
   return (
     <div className="mx-auto my-10 w-full max-w-screen-xl px-4 flex flex-row lg:justify-between justify-around gap-x-3">
       <section className="lg:w-1/4 lg:block hidden  h-full border border-gray-300  rounded-md p-2">
@@ -123,13 +219,25 @@ const RecipeSearchPage = () => {
           <span className="pb-3 font-bold">search:</span>
           <div className="flex items-center justify-center gap-x-2">
             <RecipeInput
+              defaultValue={router.query.q?.toString() ?? ""}
+              onChange={(event) => setSearchedQuery(event.target.value)}
               placeholder="search by...."
+              value={searchedQuery}
               width="w-full"
               height="h-[40px]"
             />
-            <div className="bg-primary rounded-md px-3 py-2">
+            <button
+              onClick={() =>
+                router.push({
+                  query: {
+                    q: searchedQuery,
+                  },
+                })
+              }
+              className="bg-primary rounded-md px-3 py-2"
+            >
               <i className=" fas fa-search text-white cursor-pointer"></i>
-            </div>
+            </button>
           </div>
 
           <div>
@@ -137,9 +245,20 @@ const RecipeSearchPage = () => {
               <div>
                 <div className="pt-3 font-bold">{sidebarItem.title}</div>
                 {sidebarItem.children.map((item) => (
-                  <div className="flex items-center justify-start gap-x-2 cursor-pointer">
+                  <div
+                    onClick={() =>
+                      handleSearchFilters(sidebarItem.key, item.key)
+                    }
+                    className="flex items-center justify-start gap-x-2 cursor-pointer"
+                  >
                     <div>
-                      <i className="fa-regular fa-square text-xl "></i>
+                      <i
+                        className={`fa-regular  ${
+                          handleUpdateChecklistIcon(sidebarItem.key, item.key)
+                            ? "fa-square"
+                            : "fa-square-check"
+                        } text-xl`}
+                      ></i>
                     </div>
                     <div className="">{item.title}</div>
                   </div>
@@ -149,6 +268,7 @@ const RecipeSearchPage = () => {
           </div>
           <div className="pt-5">
             <RecipeButton
+              onClick={() => handleApplyFilter()}
               UIType="secondary"
               width="w-full"
               mainText="Apply Filters"
@@ -157,12 +277,17 @@ const RecipeSearchPage = () => {
         </div>
       </section>
       <section className="lg:w-3/4 w-4/4">
+        <div className=" flex-row items-center  lg:hidden flex  gap-x-2">
+          <i className="fa-regular fa-filter text-lg"></i>
+          <button className="font-bold text-xl my-4">Filters</button>
+        </div>
         <div className="grid lg:grid-cols-3 md:grid-cols-3 grid-cols-2 gap-x-2 gap-y-4 items-center lg:justify-between  w-full">
-          {recipeSearchItems &&
+          {!loading &&
+            recipeSearchItems &&
             recipeSearchItems.hits.map(({recipe, _links}) => (
               <RecipeItem key={recipe.label} _links={_links} recipe={recipe} />
             ))}
-          {!recipeSearchItems &&
+          {loading &&
             muckArrayForSkeleton.map((item, index) => (
               <RecipeItemSkeleton key={index} />
             ))}
