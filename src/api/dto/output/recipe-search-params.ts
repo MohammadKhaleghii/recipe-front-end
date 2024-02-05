bdc4e1b5e2b223eed7f995a48bc9a37d63a0dd22
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-02-05 13:18:26 +0330

    update recipe search params interface

--- src/api/dto/output/recipe-search-params.ts
+++ src/api/dto/output/recipe-search-params.ts
@@ -1,13 +1,7 @@
 import {ParamsBase} from "../shared/params-base";
 
 export interface RecipeSearchParams extends ParamsBase {
-  diet:
-    | "balanced"
-    | "high-fiber"
-    | "high-protein"
-    | "low-carb"
-    | "low-fat"
-    | "low-sodium";
+  diet?: string;
   // health: Health;
   // cuisineType: CuisineType;
   // mealType: MealType;
