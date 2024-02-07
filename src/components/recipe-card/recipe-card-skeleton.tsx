2e198dc84ff7caba53be6214c01b3e2e65b33ae3
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-02-07 09:49:45 +0330

    add recipe skeleton ui

--- src/components/recipe-card/recipe-card-skeleton.tsx
+++ src/components/recipe-card/recipe-card-skeleton.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Skeleton from "react-loading-skeleton";
 
 type RecipeCardSkeletonProps = {};
 
 const RecipeCardSkeleton = (props: RecipeCardSkeletonProps) => {
-  return <div>RecipeCardSkeleton</div>;
+  return (
+    <div className="flex w-full flex-row gap-x-3">
+      <div className="w-[55%] ">
+        {" "}
+        <Skeleton height={100} />{" "}
+      </div>
+      <div className="w-full">
+        <Skeleton count={3} />{" "}
+      </div>
+    </div>
+  );
 };
+
+export default RecipeCardSkeleton;
