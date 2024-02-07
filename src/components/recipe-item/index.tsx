e6e5b3514ccf5773995ed0dcbda18cb801053fa6
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-02-07 09:38:19 +0330

    update tailwind class order and add component description

--- src/components/recipe-item/index.tsx
+++ src/components/recipe-item/index.tsx
@@ -1,25 +1,29 @@
-import {FC} from "react";
+import { FC } from "react";
 import Link from "next/link";
-import {RecipeHit} from "@/api/dto/input/recipe-search";
+import { RecipeHit } from "@/api/dto/input/recipe-search";
 
-const RecipeItem: FC<RecipeHit> = ({_links, recipe}) => {
+//Description
+// 1. This Component is a pure component
+// 2. We use this component for recipe item such as recent post, search page and etc.
+
+const RecipeItem: FC<RecipeHit> = ({ _links, recipe }) => {
   const recipeID = _links.self.href.split(
-    "https://api.edamam.com/api/recipes/v2/"
+    "https://api.edamam.com/api/recipes/v2/",
   );
 
   return (
     <Link
       href={`/details/${recipeID[1]}`}
-      className="flex relative items-center justify-center flex-col h-auto w-auto group transition delay-150 lg:min-h-80 min-h-32 "
+      className="group relative flex h-auto min-h-32 w-auto flex-col items-center justify-center transition delay-150 lg:min-h-80"
     >
       <img
         src={recipe.image}
         alt={recipe.label}
-        className="rounded-md w-full hover:blur-sm duration-300 transition-all relative"
+        className="relative w-full rounded-md transition-all duration-300 hover:blur-sm"
       />
 
-      <div className="absolute w-full h-full bg-black opacity-20 hover:bg-secondary rounded-lg"></div>
-      <h3 className="font-bold pt-3 lg:text-xl text-base px-2 absolute bottom-5 left-3 text-white transition ease-in-out group-hover:-translate-y-3">
+      <div className="absolute h-full w-full rounded-lg bg-black opacity-20 hover:bg-secondary"></div>
+      <h3 className="absolute bottom-5 left-3 px-2 pt-3 text-base font-bold text-white transition ease-in-out group-hover:-translate-y-3 lg:text-xl">
         <Link href={`/details/${recipeID[1]}`}>{recipe.label}</Link>
       </h3>
     </Link>
