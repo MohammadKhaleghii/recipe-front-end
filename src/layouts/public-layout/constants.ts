346f31797c2d551b61fbd2bbb3de97e632940a3a
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-02-10 10:02:46 +0330

    add layout constants

---
+++ src/layouts/public-layout/constants.ts
@@ -0,0 +1,87 @@
+export const headerNavBarItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Search",
+    href: "/search",
+  },
+  {
+    title: "About",
+    href: "https://anothermohammad.ir/",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
+export const footerCompanyItems = [
+  {
+    title: "About",
+    href: "https://anothermohammad.ir/",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+  {
+    title: "Brand Center",
+    href: "/brand-center",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+];
+export const footerHelpCenter = [
+  {
+    title: "Discord Server",
+    href: "#",
+  },
+  {
+    title: "Twitter",
+    href: "#",
+  },
+  {
+    title: "Facebook",
+    href: "#",
+  },
+  {
+    title: "Contact Us",
+    href: "#",
+  },
+];
+export const footerLegal = [
+  {
+    title: "Privacy Policy",
+    href: "#",
+  },
+  {
+    title: "Licensing",
+    href: "#",
+  },
+
+  {
+    title: "Terms & Conditions",
+    href: "#",
+  },
+];
+export const footerDownload = [
+  {
+    title: "iOS",
+    href: "#",
+  },
+  {
+    title: "Android",
+    href: "#",
+  },
+  {
+    title: "Windows",
+    href: "#",
+  },
+  {
+    title: "MacOS",
+    href: "#",
+  },
+];
