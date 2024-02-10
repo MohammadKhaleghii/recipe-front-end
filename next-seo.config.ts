1932ea44a9c91410603e7614adcfc855679e03a5
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-02-10 11:13:27 +0330

    added next seo config

---
+++ next-seo.config.ts
@@ -0,0 +1,10 @@
+const SEO = {
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://recipe-front-end-coral.vercel.app/",
+    siteName: "Food Recipe",
+  },
+};
+
+export default SEO;
