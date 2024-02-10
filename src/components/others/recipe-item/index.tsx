6d1c30c2dad7f4ccc606611bc5c570444c535228
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-02-10 15:10:39 +0330

    fixed hydration error

--- src/components/others/recipe-item/index.tsx
+++ src/components/others/recipe-item/index.tsx
@@ -1,19 +1,22 @@
 import { FC } from "react";
 import Link from "next/link";
 import { RecipeHit } from "@/api/dto/input/recipe-search";
+import { useRouter } from "next/router";
 
 //Description
 // 1. This Component is a pure component
 // 2. We use this component for recipe item such as recent post, search page and etc.
 
 const RecipeItem: FC<RecipeHit> = ({ _links, recipe }) => {
+  const router = useRouter();
   const recipeID = _links.self.href.split(
     "https://api.edamam.com/api/recipes/v2/",
   );
+  const path = `/details/${recipeID[1]}`;
 
   return (
-    <Link
-      href={`/details/${recipeID[1]}`}
+    <button
+      onClick={() => router.push(path)}
       className="group relative flex h-auto min-h-32 w-auto flex-col items-center justify-center transition delay-150 lg:min-h-80"
     >
       <img
@@ -24,9 +27,9 @@ const RecipeItem: FC<RecipeHit> = ({ _links, recipe }) => {
 
       <div className="absolute h-full w-full rounded-lg bg-black opacity-20 hover:bg-secondary"></div>
       <h3 className="absolute bottom-5 left-3 px-2 pt-3 text-base font-bold text-white transition ease-in-out group-hover:-translate-y-3 lg:text-xl">
-        <Link href={`/details/${recipeID[1]}`}>{recipe.label}</Link>
+        <Link href={path}>{recipe.label}</Link>
       </h3>
-    </Link>
+    </button>
   );
 };
 
