866a1dc19331892ad9c0ce3b886a5c1a42af03f2
mhmdkhaleghie <mhmdkhaleghie@gmail.com>
2025-05-23 13:16:32 +0330

    add tailwind to story preview config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,14 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from "@storybook/react";
+
+// import tailwind to story
+import "../src/styles/globals.css";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
